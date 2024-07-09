98877b3069d49d54eb463e1c000cd6285837edd2
raashi31 <raashiaggarwal21@gmail.com>
2024-07-10 00:18:32 +0530

    feat: modal with custom checkboxes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const pagesData = [{title: "Page 1"}, {title: "Page 2"}, {title: "Page 3"}, {title: "Page 4"}];
+
 function App() {
+  const [pages, setPages] = useState([]);
+
+  useEffect(() => {
+    setPages(pagesData);
+  }, []);
+
+  //handling all the select/unselect changes on checkboxes
+  const handleChange = (e) => {
+    const {name, checked} = e.target;
+    if(name === "allPages") {
+      let tempPages = pages.map((page) => {return {...page, isChecked : checked}});
+      setPages(tempPages);
+    } else {
+      let tempPages = pages.map((page) => page.title === name ? {...page, isChecked : checked} : page);
+      setPages(tempPages);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Text</h1>
+      <div className="modal">
+        <div className="checkbox-container">
+          <label className="checkbox-label">All pages</label>
+          <input 
+            type="checkbox" 
+            name="allPages"
+            onChange={handleChange}
+            checked={pages.filter(page => page?.isChecked !== true). length < 1}
+          />
+        </div>
+        <div className="horizontal-line"/>
+        {pages.map((page) => (
+          <div className="checkbox-container">
+            <label className="checkbox-label">{page.title}</label>
+            <input 
+              type="checkbox" 
+              name={page.title} 
+              onChange={handleChange}
+              checked={page?.isChecked || false}
+            />
+          </div>
+        ))}
+        <div className="horizontal-line"/>
+        <button type="button" className="done-button">Done</button>
+      </div>
     </div>
   );
 }
